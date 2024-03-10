2753d4fa9a82707502ce93192e005b8abc0cad53
Jacobguttl <jacobguttl2006@gmail.com>
2024-03-10 15:52:11 +0100

    o

--- AwesomeProject/assets/pages/FitPage/FitPage.js
+++ AwesomeProject/assets/pages/FitPage/FitPage.js
@@ -10,11 +10,13 @@ import {
 } from "react-native";
 import { useState } from "react";
 
+
 export default function FoodPage() {
   const [data, setData] = useState("");
   const [dataPerServing, setDataPerServing] = useState("");
   const [food, setFood] = useState("");
   var deviceWidth = Dimensions.get("window").width;
+  var deviceHeight = Dimensions.get("window").height;
 
   const handleFoodInputChange = (text) => {
     setFood(text);
@@ -36,65 +38,14 @@ export default function FoodPage() {
   const styles = StyleSheet.create({
     container: {
       backgroundColor: "blue",
-      alignItems: "center",
-      justifyContent: "center",
-      flex: 1,
-      display: "inline",
-      width: deviceWidth,
+      height: deviceHeight,
+      width: deviceHeight,
     },
-    input: {
-      borderWidth: 1,
-      borderRadius: 20,
-      padding: 8,
-      margin: 10,
-      backgroundColor: "white",
-      width: deviceWidth,
-    },
-    Text: {
-      color: "black",
-    },
-    button: {
-      backgroundColor: "white",
-      color: "black",
-    },
-    /* image: {
-      width: 20,
-      height: 20,
-    },*/
   });
   return (
     <>
       <View style={styles.container}>
-        <TextInput
-          style={styles.input}
-          placeholder="Search for action"
-          onChangeText={(food) => handleFoodInputChange(food)}
-        />
-        <Button
-          style={styles.button}
-          onPress={fetchData}
-          title="search"
-        ></Button>
-        <Text style={styles.Text}>
-          food you searched for{" "}
-          {dataPerServing.text && <Text>{dataPerServing.text}</Text>}
-        </Text>
-        <>
-          {/*       <Image source={{ uri: "./favicon.png" }} style={styles.image} />
-           */}
-        </>
-        {data && data.nutrients && (
-          <View>
-            <Text style={styles.Text}>Nutrients for 100g of food:</Text>
-            <Text style={styles.Text}>
-              ENERC_KCAL: {data.nutrients.ENERC_KCAL}
-            </Text>
-            <Text style={styles.Text}>PROCNT: {data.nutrients.PROCNT}</Text>
-            <Text style={styles.Text}>FAT: {data.nutrients.FAT}</Text>
-            <Text style={styles.Text}>CHOCDF: {data.nutrients.CHOCDF}</Text>
-            <Text style={styles.Text}>FIBTG: {data.nutrients.FIBTG}</Text>
-          </View>
-        )}
+
       </View>
     </>
   );
--- AwesomeProject/assets/pages/FoodPage/FoodPage.js
+++ AwesomeProject/assets/pages/FoodPage/FoodPage.js
@@ -7,6 +7,8 @@ import {
   TextInput,
   Button,
   Dimensions,
+  AsyncStorage,
+  TouchableHighlight,
 } from "react-native";
 import { useState } from "react";
 
@@ -14,18 +16,50 @@ export default function FoodPage() {
   const [data, setData] = useState("");
   const [dataPerServing, setDataPerServing] = useState("");
   const [food, setFood] = useState("");
-  const [deviceWidth, setDeviceWidth] = useState(Dimensions.get("window").width);
-  const [deviceHeight, setDeviceHeight] = useState(Dimensions.get("window").height);
+  const [deviceWidth, setDeviceWidth] = useState(
+    Dimensions.get("window").width
+  );
+  const [deviceHeight, setDeviceHeight] = useState(
+    Dimensions.get("window").height
+  );
   const [flexdirection, setFlexdirection] = useState("row");
+  const [visible, setVisible] = useState("none");
   const [calories, setCalories] = useState(0);
+  const [fats, setFats] = useState(0);
+  const [proteins, setProteins] = useState(0);
+  const [carbs, setCarbs] = useState(0);
+  const [fibers, setFibers] = useState(0);
 
   const handleFoodInputChange = (text) => {
     setFood(text);
   };
 
   function addingData() {
-    if (data && data.nutrients && data.nutrients.ENERC_KCAL) {
+    if (data && data.nutrients /*&& data.nutrients.ENERC_KCAL*/) {
       setCalories(calories + data.nutrients.ENERC_KCAL);
+      setFats(fats + data.nutrients.FAT);
+      setProteins(proteins + data.nutrients.PROCNT);
+      setCarbs(carbs + data.nutrients.CHOCDF);
+      setFibers(fibers + data.nutrients.FIBTG);
+    }
+  }
+  function removeData() {
+    if (data && data.nutrients /*&& data.nutrients.ENERC_KCAL*/) {
+      if (calories >= data.nutrients.ENERC_KCAL) {
+        setCalories(calories - data.nutrients.ENERC_KCAL);
+      }
+      if (fats >= data.nutrients.FAT) {
+        setFats(fats - data.nutrients.FAT);
+      }
+      if (proteins >= data.nutrients.PROCNT) {
+        setProteins(proteins - data.nutrients.PROCNT);
+      }
+      if (carbs >= data.nutrients.CHOCDF) {
+        setCarbs(carbs - data.nutrients.CHOCDF);
+      }
+      if (fibers >= data.nutrients.FIBTG) {
+        setFibers(fibers - data.nutrients.FIBTG);
+      }
     }
   }
 
@@ -37,49 +71,82 @@ export default function FoodPage() {
       const jsonData = await response.json();
       setDataPerServing(jsonData);
       setData(jsonData.parsed[0].food);
+      setVisible("inline");
     } catch (error) {
       console.error("There was an error fetching the data:", error);
     }
   };
 
+  const saveDataDaily = async (key, calories, proteins, fats) => {
+    try {
+      const time = new Date().getTime();
+      await AsyncStorage.setItem(
+        key,
+        JSON.stringify({ calories, proteins, fats,fibers, carbs, time })
+      );
+    } catch (error) {
+      "error pri ukladani", error;
+    }
+  };
+
+  const deleteDataDaily = () => {
+    setCalories(0);
+    setProteins(0);
+    setFats(0);
+    setCarbs(0);
+    setFibers(0);
+  };
+
+  const deleteOldData = () => {};
+
+  setInterval(saveDataDaily, 24 * 60 * 60 * 1000);
+  setInterval(deleteDataDaily, 24 * 60 * 60 * 1000);
+  setInterval(deleteOldData, 24 * 60 * 60 * 1000);
+
   const styles = StyleSheet.create({
     container: {
-      backgroundColor: "green",
+      backgroundColor: "yellow",
       alignItems: "center",
       width: deviceWidth,
       height: deviceHeight,
     },
     input: {
-      borderWidth: 1,
-      padding: 8,
-      marginTop: 20,
+      marginTop: deviceHeight * 0.05,
       backgroundColor: "white",
       width: deviceWidth,
+      paddingTop: 8,
+      paddingBottom: 8
     },
     textLook: {
-      color: "black",
+      color: "white",
     },
     button: {
-      backgroundColor: "white",
+      backgroundColor: "#A4A4A4",
       color: "black",
+      width: deviceWidth,
+      height: 30,
+      alignItems: "center",
+      justifyContent: "center",
     },
     fetchedFood: {
-      flex: 1,
-      justifyContent: "center",
+      justifyContent: "space-between",
       alignItems: "center",
       flexDirection: flexdirection,
       width: deviceWidth,
-      backgroundColor: "white",
+      backgroundColor: "#A4A4A4",
+      display: visible,
+      height: "30%",
+      marginTop: 20,
     },
     Button2: {
-      width: "33%",
-      height: "20%",
-      backgroundColor: "grey",
+      backgroundColor: "white",
+      height: "90%",
+      width: "30%",
+      justifyContent: "center",
+      alignItems: "center",
+
     },
-    fetchedText: {
-      width: "33%",
-      height: "20%",
-    },
+    fetchedText: { color: "white" },
   });
 
   return (
@@ -90,34 +157,46 @@ export default function FoodPage() {
           placeholder="Search for food"
           onChangeText={handleFoodInputChange}
         />
-        <Button
-          style={styles.button}
-          onPress={fetchData}
-          title="Search"
-        />
+        <TouchableHighlight style={styles.button} onPress={fetchData}>
+          <Text>Search</Text>
+        </TouchableHighlight>
         <View style={styles.fetchedFood}>
           <View style={styles.fetchedText}>
             <Text style={styles.textLook}>
-              Food you searched for:{" "}
-              {dataPerServing.text && <Text>{dataPerServing.text}</Text>}
+              Food: {dataPerServing.text && <Text>{dataPerServing.text}</Text>}
             </Text>
             {data && data.nutrients && (
               <View>
-                <Text style={styles.textLook}>Nutrients for 100g of food:</Text>
+                <Text style={styles.textLook}>Nutrients for 100g:</Text>
                 <Text style={styles.textLook}>
-                  ENERC_KCAL: {data.nutrients.ENERC_KCAL || 'N/A'}
+                  ENERC_KCAL: {data.nutrients.ENERC_KCAL || "Unkown"}
                 </Text>
-                <Text style={styles.textLook}>PROCNT: {data.nutrients.PROCNT || 'N/A'}</Text>
-                <Text style={styles.textLook}>FAT: {data.nutrients.FAT || 'N/A'}</Text>
-                <Text style={styles.textLook}>CHOCDF: {data.nutrients.CHOCDF || 'N/A'}</Text>
-                <Text style={styles.textLook}>FIBTG: {data.nutrients.FIBTG || 'N/A'}</Text>
+                <Text style={styles.textLook}>
+                  Proteins: {data.nutrients.PROCNT || "Unkown"}
+                </Text>
+                <Text style={styles.textLook}>
+                  Fats: {data.nutrients.FAT || "Unkown"}
+                </Text>
+                <Text style={styles.textLook}>
+                  Carbohydrates: {data.nutrients.CHOCDF || "Unkown"}
+                </Text>
+                <Text style={styles.textLook}>
+                  Fibers: {data.nutrients.FIBTG || "Unkown"}
+                </Text>
               </View>
             )}
           </View>
-          <Button style={styles.Button2} title="+" onPress={addingData} />
+          <TouchableHighlight style={styles.Button2} onPress={addingData}>
+            <Text>+</Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.Button2} onPress={removeData}>
+            <Text>x</Text>
+          </TouchableHighlight>
         </View>
         <View>
-          <Text>Todays nutritions: {calories}</Text>
+          <Text>
+            Todays nutritions: Cal:{Math.round(calories)} Pro:{Math.round(proteins)} Fat:{Math.round(fats)} Car:{Math.round(carbs)} Fib:{Math.round(fibers)}
+          </Text>
         </View>
       </View>
     </>
